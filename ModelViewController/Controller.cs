43150ba6dde1e1855f4bb4aac71caeb3d1ab107d
Tommy Waalkes <Tommy@grandcircus.co>
2017-10-10 20:28:06 -0400

    First Commit

---
+++ ModelViewController/Controller.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelViewController
+{
+    class Controller
+    {
+        Model m;
+        CarModel cm; 
+        View v;
+        public Controller()
+        {
+            m = new Model();
+            cm = new CarModel();
+            v = new View();
+        }
+
+        public void DisplayNames()
+        {
+            v.DisplayNames(m.GetAllNames());
+
+        }
+    }
+}
